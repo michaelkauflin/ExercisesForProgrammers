cca6138782565234301de12eaac5561e29895c5b
Kauflin, Michael <michael.kauflin@sonova.com>
2016-01-21 14:51:25 +0100

    1st commit

---
+++ TipCaclulator/TipCaclulator.Console/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using TipCalculator;
+
+namespace TipCaclulator.Console
+{
+    class Program
+    {
+        static void Main()
+        {
+            System.Console.Write("What is the bill? $");
+            string bill = System.Console.ReadLine();
+
+            System.Console.Write("What is the tip percentage? ");
+            string percentage = System.Console.ReadLine();
+
+            var engine = new Engine(bill, percentage);
+
+            System.Console.WriteLine("The tip is $" + engine.Tip);
+            System.Console.WriteLine("The total is $" + engine.Total);
+
+            System.Console.ReadLine();
+        }
+    }
+}
---
+++ TipCaclulator/TipCalculator/Engine.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TipCalculator
+{
+    public class Engine
+    {
+        private readonly string _bill;
+        private readonly string _percentage;
+
+        public Engine(string bill, string percentage)
+        {
+            _bill = bill;
+            _percentage = percentage;
+        }
+
+        public double Tip { get; private set; }
+        public double Total { get; private set; }
+    }
+}
